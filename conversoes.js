3c46281b04a8caa21cf53fc6978954856ff5d70f
88nobeat <88nobeat@gmail.com>
2021-10-15 12:56:56 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tido de dado
+//booleanos
+
+//conversao implicita
+
+    const numero = 456;
+    const numeroString = Number("456");
+
+    //Number()
+    //String()
+
+    console.log(numero +numeroString)
+
+//conversao explicita
