39f582a55975e1832eaf05152de3cf691085203f
88nobeat <88nobeat@gmail.com>
2021-10-18 13:54:47 -0300

    operadores e funções

---
+++ expressoes-funcao.js
@@ -0,0 +1,32 @@
+//declaração de função
+
+function minhaFuncao(param){
+    //bloco codigo
+}
+
+//minhaFuncao("param")
+
+//expressão de função
+
+//const soma = function(num1, num2) {
+    //return num1 +num2;
+//}
+
+//console.log(soma(3,4))
+
+// diferença principal ??????
+//funções e var sar elevados "listados" no topo
+
+console.log(apresentar())
+
+function apresentar(){
+    return "Olá";
+}
+
+
+
+
+const soma = function(num1, num2) {
+    return num1 +num2;
+}
+console.log(soma(1,1))
